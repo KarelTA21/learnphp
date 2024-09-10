b3e45b18e434c4c228f2836a79ef03fdae33dd16
KarelTA21 <karel.berg@tptlive.ee>
2024-09-10 10:53:17 +0300

    magic methods

--- index.php
+++ index.php
@@ -2,14 +2,29 @@
 
 class Box {
     public $var;
+    public $foo;
+    public $bar;
+    public function __construct($var, $foo, $bar) {
+        var_dump($var, $foo, $bar);
+        $this->var = $var;
+        $this->foo = $foo;
+        $this->bar = $bar;
+    }
+    public function __set( $name, $value ) {
+        var_dump($name, $value);
+    }
+    public function __get( $name ) {
+        return "prop $name does not exist";
+    }
+    public function __call($name, $args){
+        var_dump($name, $args);
+    }
+    public function __destruct() {
+        echo "Object was destroyed\n";
+    }
 }
-$num1 = 1;
-$num2 = $num1;
-$num2 = 2;
-var_dump( $num1, $num2 );
-
-$box1 = new Box();
-$box1->var = 1;
-$box2 = clone $box1;
-$box2->var = 2;
-var_dump($box1->var, $box2->var);
+$box = new Box('hello', 1, 2);
+$box->bla = 5;
+var_dump($box->salsa);
+$box->helloworld('Hello world');
+var_dump(serialize($box));
