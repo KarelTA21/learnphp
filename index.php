d1a2635b667876f06488e24d077a769badb9cbc4
KarelTA21 <karel.berg@tptlive.ee>
2024-09-10 10:21:17 +0300

    objects pass by reference

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-$i = 0;
+class Box {
+    public $var;
+}
+$num1 = 1;
+$num2 = $num1;
+$num2 = 2;
+var_dump( $num1, $num2 );
+
+$box1 = new Box();
+$box1->var = 1;
+$box2 = clone $box1;
+$box2->var = 2;
+var_dump($box1->var, $box2->var);
